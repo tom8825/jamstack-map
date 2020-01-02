a7ddcbec1f260fa05f8398d6b61de095f91441c3
tom.parker <tom.parker@everest-media.co.uk>
2020-01-02 10:11:11 +0000

    added reddit search api for park poi

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 var map;
 
 $(document).ready(function() {
-  $("#tripadvisor-info, #menu-info, #disqus_thread").fadeOut(500);
+  $("#tripadvisor-info, #menu-info, #disqus_thread, #reddit-info").css(
+    "display",
+    "none"
+  );
 });
 
 $("#map-info-nav-item-comments").click(function() {
-  $("#tripadvisor-info, #menu-info").fadeOut(500, function() {
+  $("#tripadvisor-info, #menu-info, #reddit-info").fadeOut(500, function() {
     $("#disqus_thread")
       .delay(500)
       .fadeIn();
@@ -13,15 +16,22 @@ $("#map-info-nav-item-comments").click(function() {
 });
 
 $("#map-info-nav-item-tripadvisor").click(function() {
-  $("#disqus_thread, #menu-info").fadeOut(500, function() {
+  $("#disqus_thread, #menu-info, #reddit-info").fadeOut(500, function() {
     $("#tripadvisor-info")
       .delay(500)
       .fadeIn();
   });
 });
 $("#map-info-nav-item-menu").click(function() {
-  $("#tripadvisor-info, #disqus_thread").fadeOut(500, function() {
+  $("#tripadvisor-info, #disqus_thread, #reddit-info").fadeOut(500, function() {
     $("#menu-info")
+      .delay(500)
+      .fadeIn();
+  });
+});
+$("#map-info-nav-item-reddit").click(function() {
+  $("#tripadvisor-info, #menu-info, #disqus_thread").fadeOut(500, function() {
+    $("#reddit-info")
       .delay(500)
       .fadeIn();
   });
@@ -52,28 +62,32 @@ function initMap() {
           '<img src="' + place.photos[1].getUrl() + '">'
         );
         $("#map-info-nav").html();
+
+        $.getJSON(
+          "https://www.reddit.com/r/WaltDisneyWorld/search.json?q=title:" +
+            place.name +
+            "&sort=new&restrict_sr=on&limit=5",
+          function(result) {
+            var data = result.data.children;
+            if (result) {
+              $("#reddit-info").html("");
+              for (var i = 0; i < data.length; i++) {
+                $("#reddit-info").append(
+                  "<li class='reddit-list-item'><a href='" +
+                    data[i].data.url +
+                    "' target='_blank'>" +
+                    data[i].data.title +
+                    "</a></li></br>"
+                );
+                console.log(data[i].data.title);
+              }
+            } else {
+              $("#reddit-info").html("");
+              $("#reddit-info").append("Sorry folks, no results found!");
+            }
+          }
+        );
       });
     }
   });
 }
-
-const Themeparks = require("themeparks");
-
-const DisneyWorldMagicKingdom = new Themeparks.Parks.WaltDisneyWorldMagicKingdom();
-
-const CheckWaitTimes = () => {
-  DisneyWorldMagicKingdom.GetWaitTimes()
-    .then(rideTimes => {
-      rideTimes.forEach(ride => {
-        console.log(
-          `${ride.name}: ${ride.waitTime} minutes wait (${ride.status})`
-        );
-      });
-    })
-    .catch(error => {
-      console.error(error);
-    })
-    .then(() => {
-      setTimeout(CheckWaitTimes, 500 * 60 * 5); // refresh every 5 minutes
-    });
-};
