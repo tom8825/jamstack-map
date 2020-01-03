e79e579544bc4d063e06babc70724d7c47d454e2
tom.parker <tom.parker@everest-media.co.uk>
2020-01-03 10:51:57 +0000

    added google ratings and various style changes

--- js/script.js
+++ js/script.js
@@ -1,40 +1,58 @@
 var map;
 
 $(document).ready(function() {
-  $("#tripadvisor-info, #menu-info, #disqus_thread, #reddit-info").css(
-    "display",
-    "none"
-  );
+  $(
+    "#tripadvisor-info, #menu-info, #disqus_thread, #reddit-info, #poi-info"
+  ).css("display", "none");
 });
 
 $("#map-info-nav-item-comments").click(function() {
-  $("#tripadvisor-info, #menu-info, #reddit-info").fadeOut(500, function() {
-    $("#disqus_thread")
-      .delay(500)
-      .fadeIn();
-  });
+  $("#tripadvisor-info, #menu-info, #reddit-info, #poi-info").fadeOut(
+    500,
+    function() {
+      $("#disqus_thread")
+        .delay(500)
+        .fadeIn();
+    }
+  );
 });
 
 $("#map-info-nav-item-tripadvisor").click(function() {
-  $("#disqus_thread, #menu-info, #reddit-info").fadeOut(500, function() {
-    $("#tripadvisor-info")
-      .delay(500)
-      .fadeIn();
-  });
+  $("#disqus_thread, #menu-info, #reddit-info, #poi-info").fadeOut(
+    500,
+    function() {
+      $("#tripadvisor-info")
+        .delay(500)
+        .fadeIn();
+    }
+  );
 });
-$("#map-info-nav-item-menu").click(function() {
-  $("#tripadvisor-info, #disqus_thread, #reddit-info").fadeOut(500, function() {
-    $("#menu-info")
-      .delay(500)
-      .fadeIn();
-  });
+$("#map-info-nav-item-info").click(function() {
+  $("#tripadvisor-info, #disqus_thread, #reddit-info, #menu-info").fadeOut(
+    500,
+    function() {
+      $("#poi-info")
+        .delay(500)
+        .fadeIn();
+    }
+  );
 });
 $("#map-info-nav-item-reddit").click(function() {
-  $("#tripadvisor-info, #menu-info, #disqus_thread").fadeOut(500, function() {
-    $("#reddit-info")
-      .delay(500)
-      .fadeIn();
-  });
+  $("#tripadvisor-info, #menu-info, #disqus_thread, #poi-info").fadeOut(
+    500,
+    function() {
+      $("#reddit-info")
+        .delay(500)
+        .fadeIn();
+    }
+  );
+});
+let menu_link = "";
+$("#map-info-nav-item-menu").click(function() {
+  window.open(
+    "https://disneyworld.disney.go.com/en_GB/dining/magic-kingdom/" + menu_link,
+    "_blank"
+  );
 });
 
 function initMap() {
@@ -48,12 +66,38 @@ function initMap() {
   var placesService = new google.maps.places.PlacesService(map);
 
   map.addListener("click", function(event) {
+    menu_link = "";
     if (event.placeId) {
       event.stop();
       placesService.getDetails({ placeId: event.placeId }, function(
         place,
         status
       ) {
+        console.log(place);
+        $("#poi-info").html("");
+        for (var i = 0; i < place.reviews.length; i++) {
+          $("#poi-info").append(
+            "<div class='collapsible'><div id='collapsible-rating-" +
+              i +
+              "'></div><div class='collapsible-info'>" +
+              place.reviews[i].author_name +
+              ": <span>" +
+              place.reviews[i].relative_time_description +
+              "</span></div></div><div class='content'><p>" +
+              place.reviews[i].text +
+              "</p></div>"
+          );
+          for (var k = 0; k < place.reviews[i].rating; k++) {
+            $("#collapsible-rating-" + i).append(
+              '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f0e40a" d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>'
+            );
+          }
+        }
+        info_dropdown();
+        $("#map-info-nav-item-info").css({
+          "border-color": "lightblue"
+        });
+        $("#map-info-nav-item-info svg path").css("fill", "lightblue");
         $("#map-info-title").text(place.name);
         $("#map-info-images").html(
           '<img src="' + place.photos[1].getUrl() + '">'
@@ -63,23 +107,38 @@ function initMap() {
           place.types.includes("food") ||
           place.types.includes("restaurant")
         ) {
-          $.getJSON(
-            "https://touringplans.com/magic-kingdom/dining.json",
-            function(result) {
-              for (var i = 0; i < result.length; i++) {
-                console.log(result[i][i].name);
-                if (result[i][i].name === place.name) {
-                  $("#menu-info").html(
-                    "Click <a href='https://disneyworld.disney.go.com/en_GB/dining/magic-kingdom/" +
-                      result[i][i].permalink +
-                      "/menus/'>here</a> for the menu."
-                  );
-                } else {
-                  $("#menu-info").html("No menu found for " + place.name + ".");
-                }
+          $.getJSON("json/mk_dining.json", function(result) {
+            let found = false;
+            for (var i = 0; i < result.length; i++) {
+              if (result[i].name === place.name) {
+                console.log(result[i].name, place.name, result[i].permalink);
+                menu_link = result[i].permalink + "/menus/";
+                $("#map-info-nav-item-menu").css({
+                  "border-color": "lightgreen",
+                  "pointer-events": "all"
+                });
+                $("#map-info-nav-item-menu svg path").css("fill", "lightgreen");
+                found = true;
+                break;
               }
             }
-          );
+            if (!found) {
+              console.log("not found");
+              menu_link = "#/search=" + place.name + "/sort=relevance/";
+              $("#map-info-nav-item-menu").css({
+                "border-color": "orange",
+                "pointer-events": "all"
+              });
+              $("#map-info-nav-item-menu svg path").css("fill", "orange");
+            }
+          });
+        } else {
+          menu_link = "";
+          $("#map-info-nav-item-menu").css({
+            "border-color": "grey",
+            "pointer-events": "none"
+          });
+          $("#map-info-nav-item-menu svg path").css("fill", "grey");
         }
         $.getJSON(
           "https://www.reddit.com/r/WaltDisneyWorld/search.json?q=title:" +
@@ -97,6 +156,11 @@ function initMap() {
                     data[i].data.title +
                     "</a></li></br>"
                 );
+                $("#map-info-nav-item-reddit").css({
+                  "border-color": "#F05236",
+                  "pointer-events": "all"
+                });
+                $("#map-info-nav-item-reddit svg path").css("fill", "#F05236");
               }
             } else {
               $("#reddit-info").html("");
@@ -107,4 +171,23 @@ function initMap() {
       });
     }
   });
+}
+
+function info_dropdown() {
+  //info collabsible list
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+    console.log("test");
+  }
 }
